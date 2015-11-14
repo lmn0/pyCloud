6af89ffc03a2d0d0821a25766fe873a25991f6a3
Thejass Krishnan <tjskrishna@gmail.com>
2015-11-14 11:03:49 -0800

    Login modules changed

--- api.js
+++ api.js
@@ -1,19 +1,32 @@
 var r = require("./lib/request"),
     logger = require("./lib/logger");
 
+var mongodb = require('mongodb');
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/pyCloud';
 
 
 var Api = {
   login: function(params, res, cb) {
-    var cfg = res.locals.config;
-    r.makeRequest(
-      config = {
-        url: cfg.apiServer + "/users/login",
-        method: "PUT"
-      },
-      body = params,
-      cb);
+    
+    //WRITE THE LOGIN LOGIC HERE !
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // Get the documents collection
+    var collection = db.collection('users');
   }
-};
+  
+});
+}
+}
 
 module.exports = Api;
